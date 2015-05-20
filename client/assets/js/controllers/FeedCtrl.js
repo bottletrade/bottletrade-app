5eb3eef7736d106ce9f91b87743f5b16e1f80bf3
soumak77 <bsoumakian@gmail.com>
2015-05-19 19:30:34 -0700

    Refactoring

--- client/assets/js/controllers/FeedCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('application').controller("FeedCtrl", ["currentAuth", function(currentAuth) {
-    // currentAuth (provided by resolve) will contain the
-    // authenticated user or null if not logged in
-  }]);
-
-})();
--- client/templates/beers/beers-controller.js
+++ client/templates/beers/beers-controller.js
@@ -2,11 +2,18 @@
   'use strict';
 
   angular.module('application').controller("BeersCtrl",
-    function($scope, $state, $stateParams, Beer, BeerList) {
+    function($scope, $state, $stateParams, Beer, BeerList, BeerManager) {
       if ($stateParams.id) {
         $scope.beer = new Beer($stateParams.id);
       } else {
         $scope.beers = BeerList;
+      }
+
+      if ($stateParams.type) {
+        switch ($stateParams.type) {
+          case "cellar":
+
+        }
       }
 
       $scope.created = function(beer) {
