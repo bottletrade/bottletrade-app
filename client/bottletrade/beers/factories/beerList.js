e76e324d820d5a2326ad46c07b0c9f27d117f863
soumak77 <bsoumakian@gmail.com>
2015-06-23 17:49:30 -0700

    Extend BeerList $$added method

--- client/bottletrade/beers/factories/beerList.js
+++ client/bottletrade/beers/factories/beerList.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('bottletrade.beers').factory("BeerList",
-    function($firebaseArray, firebaseRef, BTConstants) {
+    function($firebaseArray, firebaseRef, BTConstants, Brewery) {
       // create a new service based on $firebaseArray
       var BeerList = $firebaseArray.$extend({
         "$add": function(data) {
@@ -13,6 +13,18 @@
             data.brewery = data.brewery.$id;
           }
           return $firebaseArray.prototype.$add.call(this, data);
+        },
+        "$$added": function(snapshot, prevChild) {
+          // call the super
+          var newChild = $firebaseArray.prototype.$$added.apply(this, arguments);
+
+          if (newChild) {
+            if (angular.isString(newChild.brewery)) {
+              newChild.brewery = new Brewery(newChild.brewery);
+            }
+          }
+
+          return newChild;
         },
         "$$updated": function(data) {
           // call the super
